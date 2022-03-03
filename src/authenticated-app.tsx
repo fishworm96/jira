41a06bd7429afdc9ed531f6833e668be15363578
fishworm96 <245525860@qq.com>
2022-03-03 16:20:00 +0800

    feat: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,51 @@
 import React from "react";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.header`
+  /* display: grid; */
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`;
+
+const Header = styled(Row)`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled(Row)`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  grid-area: main;
+`;
